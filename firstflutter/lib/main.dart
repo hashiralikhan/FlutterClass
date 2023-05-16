2885e996da0d45cfc9f3ea199e80e24f8a65a6cd
Rana-Hashir <hashiraliphw@gmail.com>
2023-05-16 21:14:22 +0500

    login page

--- firstflutter/lib/main.dart
+++ firstflutter/lib/main.dart
@@ -9,8 +9,30 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home:Scaffold( body: Text("hello word"),)
-
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(appBar: AppBar(
+        title: Center(child: Text("Login page")),),
+        body: Center(
+          child: Column(
+            
+            children: [
+              SizedBox(height: 50,),
+              Container(
+                width: 200,
+                child: TextField()),
+                SizedBox(height: 30,),
+                Container(
+                width: 200,
+                child: TextField()),
+                SizedBox(height: 30,),
+                ElevatedButton(onPressed: (){}, child: Text("login"))
+            ],
+          ),
+        ),
+        
+      
+      ),
+      
     );
   }
 }
