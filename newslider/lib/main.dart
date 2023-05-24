a8b2c80c52a0db4fd6a6383604f9742258473e25
Rana-Hashir <hashiraliphw@gmail.com>
2023-05-25 01:01:30 +0500

    todo

--- newslider/lib/main.dart
+++ newslider/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:newslider/tablog.dart';
+import 'package:newslider/todo.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: tablog(),
+        body: todo(),
       ),
     );
   }
---
+++ newslider/lib/todo.dart
@@ -0,0 +1,103 @@
+
+import 'package:flutter/material.dart';
+
+class todo extends StatefulWidget {
+  
+
+  @override
+  State<todo> createState() => _todoState();
+}
+
+
+class _todoState extends State<todo> {
+  var output ="";
+  List<dynamic> St= [];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Center(
+          child: Text("Todo app",
+          style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold,),),
+        ),
+      ),
+      body:
+       ListView.builder(
+        itemCount: St.length,
+        itemBuilder: (context,index){
+        return Container(
+          height: 50,
+          width: 20,
+          color: Color.fromARGB(255, 101, 180, 77),
+          margin: EdgeInsets.only(top: 5),
+          child: ListTile(
+            title: Text("${St[index]}"),
+            trailing: Container(
+              width: 50,
+              child: Row(
+                children: [ 
+                  GestureDetector(onTap:(){
+                     showDialog(context: context, builder: (context){
+          return AlertDialog(
+            title: Text("Edit item"),
+            content: TextField(
+              onChanged: (value) {
+                output=value;
+              },
+            ),
+            actions: [
+              ElevatedButton(onPressed: (){
+                
+                setState(() {
+                  St.replaceRange(index, index+1, {output});
+                });
+                        Navigator.of(context).pop();
+
+              }, child: Text("Edit"))
+            ],
+
+          );
+        });
+                  
+                    
+                  }  ,child: Icon(Icons.edit)),
+
+                  GestureDetector(onTap:(){
+                    setState(() {
+                      St.removeAt(index);
+                    });
+                  }  ,child: Icon(Icons.delete)),
+                ],
+              ),
+            ) ,
+          ),
+        );
+      }),
+
+      floatingActionButton: FloatingActionButton(onPressed: (){
+        showDialog(context: context, builder: (context){
+          return AlertDialog(
+            title: Text("add item"),
+            content: TextField(
+              onChanged: (value) {
+                output=value;
+              },
+            ),
+            actions: [
+              ElevatedButton(onPressed: (){
+                
+                setState(() {
+                  St.add(output);
+                });
+                        Navigator.of(context).pop();
+
+              }, child: Text("add"))
+            ],
+
+          );
+        });
+      },child: Text("add"),),
+    );
+  }
+}
